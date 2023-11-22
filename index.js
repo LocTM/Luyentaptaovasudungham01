85384911483f5a25244a3c5167b874297c24d4cf
LocTM <tranminhloc.anhk@gmail.com>
2023-11-22 23:49:47 +0700

    BT2

---
+++ index.js
@@ -0,0 +1,14 @@
+// BT1
+// function alertMessage(){
+//     alert("Xin chao")
+// }
+
+// BT2
+// function incrementNumber(inputNumber) {
+//     return inputNumber+1;
+// }
+// function displayResult() {
+//     let inputNumber = +(document.getElementById("inputNumber").value);
+//     let result = incrementNumber(inputNumber);
+//     document.getElementById("result").innerHTML = "Kết quả: " + result;
+// }
