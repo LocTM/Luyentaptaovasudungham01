3457d458ef1fabb03af08ca92273ce41ee588343
LocTM <tranminhloc.anhk@gmail.com>
2023-11-23 00:28:59 +0700

    done

--- index.js
+++ index.js
@@ -40,3 +40,27 @@
 // console.log(result);
 // result = addNumbers();
 // console.log(result);
+
+// BT5
+let arr1 = ["Polaris", "Aldebaran", "Deneb", "Vega", "Altair", "Dubhe", "Regulus"];
+let arr2 = ["Ursa Minor", "Taurus", "Cygnus", "Lyra", "Aquila", "Ursa Major", "Leo"];
+function findConstellation(starName) {
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] === starName) {
+            // trả lại tên chòm sao tương ứng
+            return arr2[i];
+        }
+    }
+
+    // không tìm thấy
+    return -1;
+}
+
+let userInput = prompt("Nhập tên ngôi sao cần tìm:");
+let result = findConstellation(userInput);
+if (result !== -1) {
+    alert("Ngôi sao: " + userInput + "Chòm sao tương ứng là " + result);
+} else {
+    alert("Không tìm thấy thông tin về ngôi sao: " + userInput);
+}
+
